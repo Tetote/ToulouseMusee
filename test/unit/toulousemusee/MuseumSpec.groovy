59bef52f77b11a71193358c59e770fa3e11be2e1
Théo Vaucher <tetote@free.fr>
2015-04-20 14:47:38 +0200

    Rewrite MuseumSpec with valid test data (from Musee.csv)

--- test/unit/toulousemusee/MuseumSpec.groovy
+++ test/unit/toulousemusee/MuseumSpec.groovy
@@ -22,11 +22,13 @@ class MuseumSpec extends Specification {
         museum.validate()
 
         where:
-        name    | hours     | phoneNumber   | metroAccess   | busAccess
-        "nom"   | "8h"      | "052344547"   | "Ligne B"     | "Ligne 65"
-        "nom"   | "9h"      | "052344547"   | null          | "Ligne 65"
-        "nom"   | "10h"     | "052344547"   | "Ligne B"     | null
-        "nom"   | "11h"     | "052344547"   | null          | null
+        name                               | hours                                                                 | phoneNumber   | metroAccess              | busAccess
+        "ARCHIVES MUNICIPALES TOULOUSE"    | "Ouvert du lundi au vendredi de 9h à 17h. Fermeture de 12h à 13h30" +
+                " pendant toutes les vacances scolaires. Fermeture annuelle la dernière quinzaine de juillet."     | "561616333"   | "Roseraie (A)"           | "36, 38"
+        "MUSEE DES COMPAGNONS"             | "Le Mercredi et le 1er dimanche de chaque mois de 14h à 17h"          | "562474177"   | "Esquirol, Capitole (A)" | null
+        "MUSEE DU VIEUX TOULOUSE"          | "Ouvert tous les jours du 2 mai au 31 octobre de 14h00 à 18h00." +
+                "Fermé le dimanche et jours fériés."                                                               | "562271150"   | null                     | "2, 10, 12, 14, 38, 78, 80"
+        "ENSEMBLE CONVENTUEL DES JACOBINS" | "Ouvert tous les jours de 9h à 19h."                                  | "561222192"   | null                     | null
     }
 
     @Unroll
@@ -39,12 +41,12 @@ class MuseumSpec extends Specification {
         !museum.validate()
 
         where:
-        name    | hours     | phoneNumber   | address        | manager
-        ""      | "8h"      | "052344547"   | Mock(Address)  | Mock(Manager)
-        "nom"   | ""        | "052344547"   | Mock(Address)  | Mock(Manager)
-        "nom"   | "10h"     | ""            | Mock(Address)  | Mock(Manager)
-        null    | "8h"      | "052344547"   | Mock(Address)  | Mock(Manager)
-        "nom"   | null      | "052344547"   | Mock(Address)  | Mock(Manager)
-        "nom"   | "10h"     | null          | Mock(Address)  | Mock(Manager)
+        name                                 | hours                                | phoneNumber   | address        | manager
+        ""                                   | "Ouvert tous les jours de 9h à 19h." | "561222192"   | Mock(Address)  | Mock(Manager)
+        "ENSEMBLE CONVENTUEL DES JACOBINS"   | ""                                   | "561222192"   | Mock(Address)  | Mock(Manager)
+        "ENSEMBLE CONVENTUEL DES JACOBINS"   | "Ouvert tous les jours de 9h à 19h." | ""            | Mock(Address)  | Mock(Manager)
+        null                                 | "Ouvert tous les jours de 9h à 19h." | "561222192"   | Mock(Address)  | Mock(Manager)
+        "ENSEMBLE CONVENTUEL DES JACOBINS"   | null                                 | "561222192"   | Mock(Address)  | Mock(Manager)
+        "ENSEMBLE CONVENTUEL DES JACOBINS"   | "Ouvert tous les jours de 9h à 19h." | null          | Mock(Address)  | Mock(Manager)
     }
 }
