a3935cd6eaaa67663de421bfcc84e62c94ca5407
Théo Vaucher <tetote@free.fr>
2015-04-21 16:00:36 +0200

    Add unit tests for MuseumVisitRequest

--- test/unit/toulousemusee/MuseumVisitRequestSpec.groovy
+++ test/unit/toulousemusee/MuseumVisitRequestSpec.groovy
@@ -2,6 +2,7 @@ package toulousemusee
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,12 +10,37 @@ import spock.lang.Specification
 @TestFor(MuseumVisitRequest)
 class MuseumVisitRequestSpec extends Specification {
 
-    def setup() {
+    @Unroll
+    void "test de validite d'une museumVisitRequest valide"(Date requestDate, def _) {
+        given: "une museumVisitRequest initialise avec une requestDate, un museum et une visitRequest"
+        MuseumVisitRequest museumVisitRequest = new MuseumVisitRequest(
+                requestDate: requestDate,
+                museum: Mock(Museum),
+                visitRequest: Mock(VisitRequest))
+
+        expect: "la museumVisitRequest est valide"
+        museumVisitRequest.validate()
+
+        where:
+        requestDate          | _
+        new Date(2015,02,02) | _
     }
 
-    def cleanup() {
-    }
+    @Unroll
+    void "test d'invalidite d'une museumVisitRequest non valide"(Date requestDate, Museum museum, VisitRequest visitRequest) {
+        given: "une museumVisitRequest initialise avec une requestDate, un museum et une visitRequest"
+        MuseumVisitRequest museumVisitRequest = new MuseumVisitRequest(
+                requestDate: requestDate,
+                museum: museum,
+                visitRequest: visitRequest)
 
-    void "test something"() {
+        expect: "la museumVisitRequest est non valide"
+        !museumVisitRequest.validate()
+
+        where:
+        requestDate          | museum       | visitRequest
+        null                 | Mock(Museum) | Mock(VisitRequest)
+        new Date(2015,02,02) | null         | Mock(VisitRequest)
+        new Date(2015,02,02) | Mock(Museum) | null
     }
 }
