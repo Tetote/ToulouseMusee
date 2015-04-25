72bb28c24ae0f87907c0a7b8dcaabc0bebeed127
Théo Vaucher <tetote@free.fr>
2015-04-25 13:56:52 +0200

    Call JeuTestService in BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import toulousemusee.JeuTestService
+
 class BootStrap {
 
+    JeuTestService jeuTestService
+
     def init = { servletContext ->
+
+        jeuTestService.createJeuTestForMuseum()
     }
     def destroy = {
     }
